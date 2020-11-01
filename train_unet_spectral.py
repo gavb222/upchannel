ed6e2aa70ca7af62e88a667a60527560167d398e
gavb222 <73796801+gavb222@users.noreply.github.com>
2020-11-01 18:33:39 -0500

    Update and rename train.py to train_unet_spectral.py

--- train_unet_spectral.py
+++ train_unet_spectral.py
@@ -1,6 +1,6 @@
 #TODO: Make the dataset bigger!, find the output dims of the network
 
-from network import UNet
+from network_unet_spectral import UNet
 import torch
 import torchaudio
 import torch.nn as nn
